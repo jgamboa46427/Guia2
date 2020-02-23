1fd87caa96fcab5341f4019eb201192b507fccd7
jgamboa46427 <60361426+jgamboa46427@users.noreply.github.com>
2020-02-23 12:37:41 -0500

    Add files via upload

---
+++ Serie 3 Grupal/Ejercicio 1/CalculoPromedio/CalculoPromedio.java
@@ -0,0 +1,69 @@
+
+import java.util.Scanner;
+/**
+ * CalculoPromedio
+ * 
+ * JHON GAMBOA 
+ * 1.0
+ */
+public class CalculoPromedio
+
+{
+    public static void main (String [] args)
+    {
+    
+    Scanner entrada = new Scanner(System.in);
+    System.out.print("Cuantos numeros va digitar: ");
+    int n = entrada.nextInt();
+    int promedio = 0;
+    int [] arreglo; //Declara el arreglo
+    int posicion = 0;
+    arreglo = new int [n]; //Se inicializa
+    int suma = 0;
+    int mayor = 0;
+    int menor = 0;
+    
+
+      for ( int contador = n ; contador > 0  ; contador--){
+                    System.out.print("Digite el numero: " );
+                    arreglo [posicion]= entrada.nextInt();
+                    posicion ++ ;
+
+                }
+                menor = arreglo[0];
+        for (int contador = 0 ; contador < arreglo.length ; contador++){
+                    
+                    
+                    
+               
+                    if ( mayor < arreglo[contador] ){
+                            mayor = arreglo[contador];
+                        }
+                        
+                   
+                        if ( arreglo[contador] < menor ){
+                            menor = arreglo[contador];
+                        }    
+                        
+                        
+                    
+                        
+                        suma += arreglo[contador]; 
+            
+                    
+            }
+            promedio = suma / arreglo.length;
+        System.out.println("Promedio: " + promedio); 
+        System.out.println("Mayor: " + mayor);
+        System.out.println("Menor: " + menor);
+      
+        }
+        
+      
+    
+    
+}
+
+    
+    
+
---
+++ Serie 3 Grupal/Ejercicio 2/DesviacionEstandar/DesviacionEstandar.java
@@ -0,0 +1,55 @@
+import java.util.Scanner; 
+import java.util.*;  
+/**
+ * DesviacionEstandar
+ * 
+ * JOHN GAMBOA 
+ * 1.0
+ */
+public class DesviacionEstandar
+{
+    
+    public static void main (String [] args)
+    {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println(" ***CALCULO DESVIACION ESTANDAR*** ");
+        int n = 0;
+        do {
+            System.out.print("Cuantos numeros va a digitar: ");
+            n = entrada.nextInt();
+        } while (n <= 0);
+        
+        int posicion = 0;
+        double promedio;
+        double [] arreglo = new double [n];//Declara el arreglo
+        double suma = 0;
+        double calculo = 0;
+        
+        
+        for ( int contador = n ; contador > 0  ; contador--){
+                    System.out.print("Digite el numero: " );
+                    arreglo [posicion]= entrada.nextInt();
+                    posicion ++ ;
+
+                }
+        
+        
+        for (int contador = 0 ; contador < arreglo.length ; contador++){
+            suma += arreglo[contador];
+                }
+                
+    promedio = suma / arreglo.length;
+    System.out.println("Promedio: " + promedio);
+    
+    
+        for (int contador = 0 ; contador < arreglo.length ; contador++){
+                    
+                    calculo += Math.pow((arreglo[contador]- promedio),2);
+            
+                    
+            }
+    calculo = Math.sqrt(calculo / arreglo.length);
+    System.out.print("Desviacion estandar: " + calculo);
+}
+    
+}
